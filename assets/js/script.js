e46ff95bfde26c0f3cfc4b1ff8799dcd068377a4
Deena Smith <deenasmith1630@gmail.com>
2022-03-20 17:47:23 -0500

    search for city now works and displays current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
 
-var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=42c34ed261d0d482947917bfe075d9a3';
 var searchBtn = document.getElementById('search-button');
 var currentCity = document.getElementById('current-city');
 var currentDate = document.getElementById('current-date');
 var currentTemp = document.getElementById('current-temp');
 var currentHum = document.getElementById('current-hum');
-var currentWind= document.getElementById('current-wind');
+var currentWind = document.getElementById('current-wind');
+var currentUV = document.getElementById('current-uv');
 
 
+searchBtn.addEventListener('click', function (event) {
+    let userInput = document.getElementById('cityNameBtn').value.trim()
+    let splitInput = userInput.split(" ")
+    let urlUserInput = splitInput.join('+')
+
+    var weatherUrl = 'http://api.openweathermap.org/data/2.5/forecast?q='+urlUserInput+'&appid=42c34ed261d0d482947917bfe075d9a3';
 
 
-searchBtn.addEventListener('click', function () {
     const getWeather = async () => {
         try {
             const response = await fetch(weatherUrl)
@@ -29,12 +34,15 @@ searchBtn.addEventListener('click', function () {
         currentCity.innerHTML = inputParam.city.name;
         currentDate.innerHTML = inputParam.list[0].dt_txt;
         currentTemp.innerHTML = inputParam.list[0].main.temp;
-        currentHum.innerHTML = inputParam
+        currentHum.innerHTML = inputParam.list[0].main.humidity;
         currentWind.innerHTML = inputParam.list[0].wind.speed;
+        currentUV.innerHTML = inputParam
 
         console.log(inputParam)
 
-        
+        for (let i = 0; i < 5; i++) {
+            console.log(inputParam.list[i]);
+        }
 
     
     }
